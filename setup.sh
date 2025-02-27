e0a870f953768f0e0465f19aa06411eeb6bfdd25
victor.rascle <victor.rascle@formation-technologique.fr>
2025-02-27 11:14:22 +0100

    ajout des premiers outils

--- setup.sh
+++ setup.sh
@@ -45,8 +45,14 @@ install_holehe() {
         echo "✅ holehe est déjà installé."
     else
         sudo apt install -y git
-        git clone https://github.com/megadose/holehe.git
-        cd holehe || exit 1
+        if [ -d "holehe" ]; then
+            echo "⚠️ Le dossier 'holehe' existe déjà, mise à jour..."
+            cd holehe || exit 1
+            git pull
+        else
+            git clone https://github.com/megadose/holehe.git
+            cd holehe || exit 1
+        fi
         python3 setup.py install
         cd ..
         if command_exists holehe; then
@@ -58,6 +64,48 @@ install_holehe() {
     fi
 }
 
+install_hawker() {
+    echo "⏳ Installation de Hawker..."
+    if command_exists hawker; then
+        echo "✅ Hawker est déjà installé."
+    else
+        sudo apt install -y git
+        if [ -d "Hawker" ]; then
+            echo "⚠️ Le dossier 'Hawker' existe déjà, mise à jour..."
+            cd Hawker || exit 1
+            git pull
+        else
+            git clone https://github.com/RetrO-M/Hawker.git
+            cd Hawker || exit 1
+        fi
+        pip install -r requirements.txt
+        cd ..
+        if command_exists hawker; then
+            echo "✅ Hawker installé avec succès."
+        else
+            echo "❌ Échec de l'installation de Hawker."
+            exit 1
+        fi
+    fi
+}
+
+install_h8mail() {
+    echo "⏳ Installation de h8mail..."
+    if command_exists h8mail; then
+        echo "✅ h8mail est déjà installé."
+    else
+        pip3 install h8mail
+        if command_exists h8mail; then
+            echo "✅ h8mail installé avec succès."
+        else
+            echo "❌ Échec de l'installation de h8mail."
+            exit 1
+        fi
+    fi
+}
+
 install_pip
 install_pipx
 install_holehe
+install_hawker
+install_h8mail
