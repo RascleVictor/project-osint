7642bac508a346186ea7d6c13ba7b2250f388ea3
victor <victor.rascle@lyceeastier.com>
2025-03-02 15:41:01 +0100

    changement dans le code d'installe

--- setup.sh
+++ setup.sh
@@ -4,40 +4,40 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-echo "⏳ Mise à jour des paquets système..."
-sudo apt update && sudo apt upgrade -y
+#echo "⏳ Mise à jour des paquets système..."
+#sudo apt update && sudo apt upgrade -y
 
-install_pip() {
-    echo "⏳ Vérification de pip3..."
-    if command_exists pip3; then
-        echo "✅ pip3 est déjà installé : $(pip3 --version)"
-    else
-        echo "⚠️ pip3 n'est pas installé. Installation en cours..."
-        sudo apt install -y python3-pip
-        if command_exists pip3; then
-            echo "✅ pip3 installé avec succès : $(pip3 --version)"
-        else
-            echo "❌ Échec de l'installation de pip3."
-            exit 1
-        fi
-    fi
-}
+#install_pip() {
+#    echo "⏳ Vérification de pip3..."
+#    if command_exists pip3; then
+#        echo "✅ pip3 est déjà installé : $(pip3 --version)"
+#    else
+#        echo "⚠️ pip3 n'est pas installé. Installation en cours..."
+#        sudo apt install -y python3-pip
+#        if command_exists pip3; then
+#            echo "✅ pip3 installé avec succès : $(pip3 --version)"
+#        else
+#            echo "❌ Échec de l'installation de pip3."
+#            exit 1
+#        fi
+#    fi
+#}
 
-install_pipx() {
-    echo "⏳ Vérification de pipx..."
-    if command_exists pipx; then
-        echo "✅ pipx est déjà installé : $(pipx --version)"
-    else
-        echo "⚠️ pipx n'est pas installé. Installation en cours..."
-        sudo apt install pipx
-        if command_exists pipx; then
-            echo "✅ pipx installé avec succès : $(pipx --version)"
-        else
-            echo "❌ Échec de l'installation de pipx."
-            exit 1
-        fi
-    fi
-}
+#install_pipx() {
+#    echo "⏳ Vérification de pipx..."
+#    if command_exists pipx; then
+#        echo "✅ pipx est déjà installé : $(pipx --version)"
+#    else
+#        echo "⚠️ pipx n'est pas installé. Installation en cours..."
+#        sudo apt install pipx
+#        if command_exists pipx; then
+#            echo "✅ pipx installé avec succès : $(pipx --version)"
+#        else
+#            echo "❌ Échec de l'installation de pipx."
+#            exit 1
+#        fi
+#    fi
+#}
 
 install_holehe() {
     echo "⏳ Installation de holehe..."
@@ -108,12 +108,7 @@ install_zehef() {
     if command_exists zehef; then
         echo "✅ Zehef est déjà installé."
     else
-        if [ -d "Zehef" ]; then
-            echo "⚠️ Le dossier 'Zehef' existe déjà, mise à jour..."
-            cd Zehef && git pull && cd ..
-        else
-            sudo git clone https://github.com/N0rz3/Zehef.git && cd Zehef && sudo python3 -m pip install -r requirements.txt && cd ..
-        fi
+        [ -d "Zehef" ] && (echo "⚠️ Le dossier 'Zehef' existe déjà, mise à jour..." && cd Zehef && git pull && cd ..) || (sudo git clone https://github.com/N0rz3/Zehef.git && cd Zehef && sudo python3 -m pip install -r requirements.txt && cd ..)
         command_exists zehef && echo "✅ Zehef installé avec succès." || { echo "❌ Échec de l'installation de Zehef."; exit 1; }
     fi
 }
@@ -123,12 +118,7 @@ install_email_osint() {
     if command_exists email-osint; then
         echo "✅ Email-Osint est déjà installé."
     else
-        if [ -d "Email-Osint" ]; then
-            echo "⚠️ Le dossier 'Email-Osint' existe déjà, mise à jour..."
-            cd Email-Osint && git pull && cd ..
-        else
-            git clone https://github.com/KanekiX2/Email-Osint.git && cd Email-Osint && sudo python3 -m pip install -r requirements.txt && cd ..
-        fi
+        [ -d "Email-Osint" ] && (echo "⚠️ Le dossier 'Email-Osint' existe déjà, mise à jour..." && cd Email-Osint && git pull && cd ..) || (git clone https://github.com/KanekiX2/Email-Osint.git && cd Email-Osint && sudo python3 -m pip install -r requirements.txt && cd ..)
         command_exists email-osint && echo "✅ Email-Osint installé avec succès." || { echo "❌ Échec de l'installation de Email-Osint."; exit 1; }
     fi
 }
@@ -138,12 +128,7 @@ install_blackbird() {
     if command_exists blackbird; then
         echo "✅ blackbird est déjà installé."
     else
-        if [ -d "blackbird" ]; then
-            echo "⚠️ Le dossier 'blackbird' existe déjà, mise à jour..."
-            cd blackbird && git pull && cd ..
-        else
-            git clone https://github.com/p1ngul1n0/blackbird && cd blackbird && sudo pip3 install -r requirements.txt && cd ..
-        fi
+        [ -d "blackbird" ] && (echo "⚠️ Le dossier 'blackbird' existe déjà, mise à jour..." && cd blackbird && git pull && cd ..) || (git clone https://github.com/p1ngul1n0/blackbird && cd blackbird && sudo python3 -m pip install -r requirements.txt && cd ..)
         command_exists blackbird && echo "✅ blackbird installé avec succès." || { echo "❌ Échec de l'installation de blackbird."; exit 1; }
     fi
 }
@@ -153,12 +138,7 @@ install_gumshoe() {
     if command_exists gumshoe; then
         echo "✅ gumshoe est déjà installé."
     else
-        if [ -d "gumshoe" ]; then
-            echo "⚠️ Le dossier 'gumshoe' existe déjà, mise à jour..."
-            cd gumshoe && git pull && cd ..
-        else
-            git clone https://github.com/asharbinkhalil/gumshoe.git && cd gumshoe && sudo pip3 install -r requirements.txt && cd ..
-        fi
+        [ -d "gumshoe" ] && (echo "⚠️ Le dossier 'gumshoe' existe déjà, mise à jour..." && cd gumshoe && git pull && cd ..) || (git clone https://github.com/asharbinkhalil/gumshoe.git && cd gumshoe && sudo python3 -m pip install -r requirements.txt && cd ..)
         command_exists gumshoe && echo "✅ gumshoe installé avec succès." || { echo "❌ Échec de l'installation de gumshoe."; exit 1; }
     fi
 }
@@ -168,12 +148,7 @@ install_mailogleit() {
     if command_exists mailogleit; then
         echo "✅ mailogleit est déjà installé."
     else
-        if [ -d "mailogleit" ]; then
-            echo "⚠️ Le dossier 'mailogleit' existe déjà, mise à jour..."
-            cd mailogleit && git pull && cd ..
-        else
-            git clone https://github.com/dincertekin/mailogleit.git && cd mailogleit && sudo pip3 install -r requirements.txt && cd ..
-        fi
+        [ -d "mailogleit" ] && (echo "⚠️ Le dossier 'mailogleit' existe déjà, mise à jour..." && cd mailogleit && git pull && cd ..) || (git clone https://github.com/dincertekin/mailogleit.git && cd mailogleit && sudo python3 -m pip install -r requirements.txt && cd ..)
         command_exists mailogleit && echo "✅ mailogleit installé avec succès." || { echo "❌ Échec de l'installation de mailogleit."; exit 1; }
     fi
 }
@@ -183,12 +158,7 @@ install_profil3r() {
     if command_exists Profil3r; then
         echo "✅ Profil3r est déjà installé."
     else
-        if [ -d "Profil3r" ]; then
-            echo "⚠️ Le dossier 'Profil3r' existe déjà, mise à jour..."
-            cd Profil3r && git pull && cd ..
-        else
-            git clone https://github.com/Rog3rSm1th/Profil3r.git && cd Profil3r && sudo python3 setup.py install && cd ..
-        fi
+        [ -d "Profil3r" ] && (echo "⚠️ Le dossier 'Profil3r' existe déjà, mise à jour..." && cd Profil3r && git pull && cd ..) || (git clone https://github.com/Rog3rSm1th/Profil3r.git && cd Profil3r && sudo python3 setup.py install && cd ..)
         command_exists Profil3r && echo "✅ Profil3r installé avec succès." || { echo "❌ Échec de l'installation de Profil3r."; exit 1; }
     fi
 }
@@ -203,17 +173,16 @@ install_philint() {
     if command_exists philINT; then
         echo "✅ philINT est déjà installé."
     else
-        git clone https://github.com/ajuelosemmanuel/philINT.git && cd philINT && python3 setup.py install && cd ..
+        [ -d "philINT" ] && (echo "⚠️ Le dossier 'philINT' existe déjà, mise à jour..." && cd philINT && git pull && cd ..) || (git clone https://github.com/ajuelosemmanuel/philINT.git && cd philINT && sudo python3 setup.py install && cd ..)
         command_exists philINT && echo "✅ philINT installé avec succès." || { echo "❌ Échec de l'installation de philINT."; exit 1; }
     fi
 }
 
 
-install_pip
-install_pipx
+
+#install_pip
+#install_pipx
 install_holehe
-#install_hawker
-#install_buster
 install_h8mail
 install_eyes
 install_zehef
